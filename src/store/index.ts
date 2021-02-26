56aec0d2f4588df759a66b2ede6a27ce721f5633
pjlapinski <przemyslaw.j.lapinski@gmail.com>
2021-02-27 00:29:35 +0100

    Made the store persist in local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { StoreModel, RemoteRepository } from '@/types';
-import { computed, createStore, createTypedHooks } from 'easy-peasy';
+import { computed, createStore, createTypedHooks, persist } from 'easy-peasy';
 
 const typedHooks = createTypedHooks<StoreModel>();
 
@@ -7,9 +7,18 @@ export const useStoreActions = typedHooks.useStoreActions;
 export const useStoreDispatch = typedHooks.useStoreDispatch;
 export const useStoreState = typedHooks.useStoreState;
 
-export default createStore<StoreModel>({
-  repositories: [],
-  currentRepository: null,
-  remoteRepositories: computed(state => state.repositories.filter(repo => 'url' in repo) as RemoteRepository[]),
-  localRepositories: computed(state => state.repositories.filter(repo => !('url' in repo))),
-});
+export default createStore<StoreModel>(
+  persist(
+    {
+      repositories: [
+        { name: 'repo1', localPath: 'C:\\repos\\repo1\\' },
+        { name: 'repo2', localPath: 'C:\\repos\\repo2\\' },
+        { name: 'repo3', localPath: 'C:\\repos\\repo3\\' },
+      ],
+      currentRepository: null,
+      remoteRepositories: computed(state => state.repositories.filter(repo => 'url' in repo) as RemoteRepository[]),
+      localRepositories: computed(state => state.repositories.filter(repo => !('url' in repo))),
+    },
+    { storage: 'localStorage' }
+  )
+);
