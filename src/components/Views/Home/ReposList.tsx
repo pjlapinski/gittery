dc0d9294ad7cb76e71568a18eccca55a8b0c8c08
pjlapinski <przemyslaw.j.lapinski@gmail.com>
2021-02-27 01:04:55 +0100

    Added a key to each repo

--- src/components/Views/Home/ReposList.tsx
+++ src/components/Views/Home/ReposList.tsx
@@ -8,8 +8,8 @@ export default function ReposList() {
   return (
     <div className='overflow-auto'>
       <div className='container pt-2'>
-        {repos.map(repo => (
-          <Link to='#' className='btn btn-block btn-outline-light border-0 m-0 p-0'>
+        {repos.map((repo, index) => (
+          <Link to='#' className='btn btn-block btn-outline-light border-0 m-0 p-0' key={index}>
             <span className='row mx-1 px-2 justify-content-between'>
               <h3>{repo.name}</h3>
               <h6 className='text-secondary align-self-end'>{repo.localPath}</h6>
