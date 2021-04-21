05e595affdcccbd0a63caf2dbd99f7f91ac975f2
pjlapinski <przemyslaw.j.lapinski@gmail.com>
2021-04-22 01:30:05 +0200

    Made the modals render on body

--- src/components/shared/ModalWindow.tsx
+++ src/components/shared/ModalWindow.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 interface ModalProps {
   id: string;
@@ -8,12 +9,13 @@ interface ModalProps {
 }
 
 const ModalWindow = ({ id, ariaLabelledby, children, className }: ModalProps) => {
-  return (
+  return ReactDOM.createPortal(
     <div id={id} className='modal' tabIndex={-1} role='dialog' aria-labelledby={ariaLabelledby} aria-hidden='true'>
       <div className='modal-dialog modal-dialog-centered' role='document'>
-        <div className={`modal-content bg-dark border border-white ${className}`}>{children}</div>
+        <div className={`modal-content bg-dark border text-white border-white ${className}`}>{children}</div>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 };
 
